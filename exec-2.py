a7cf0a158f643af9ccd9fff79d51fc8ebb1a07ef
Walker Miller <walkermillerffc@gmail.com>
2024-09-02 19:16:16 -0300

    exec-2.py

---
+++ exec-2.py
@@ -0,0 +1,5 @@
+#Exercicio 2  Elabore um programa que escreve seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
+
+print("Walker Miller")
+print("RJ")
+print("Rio de janeiro")
