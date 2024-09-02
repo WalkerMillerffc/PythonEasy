9aec002f446e34d58263f298fd5b998690e5b8d2
Walker Miller <walkermillerffc@gmail.com>
2024-09-02 19:32:40 -0300

    exec-3.py

--- exec-3.py
+++ exec-3.py
@@ -1,3 +1,4 @@
-#Exercicio 3 Faça um programa que mostre na tela uma letra de música que você gosta (proibido letras do Justin Bieber).
+3. Letra de música - Faça um programa que imprima na tela o nome de uma música que você gosta (proibido letras do Justin Bieber).
+
 
 print("That's my way and I go Esse é meu caminho nele eu vou Eu gosto de pensar que a luz do sol Vai iluminar o meu amanhecerMas se na manhã o sol não surgirPor traz das nuvens cinzas tudo vai mudar A chuva abraçará e o berço vai abrirA luz de um novo dia sempre vai estar Pra clarear você Pra iluminar você Pra proteger Pra inspirar E alimentar você")
