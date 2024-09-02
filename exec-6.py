c426fe1d24459ecfa63d31ac2a6655c9d9834dd9
Walker Miller <walkermillerffc@gmail.com>
2024-09-02 19:31:10 -0300

    exec-6.py

---
+++ exec-6.py
@@ -0,0 +1,19 @@
+
+6. Menu - Elabore um programa que mostre o seguinte menu na tela:
+Cadastro de Clientes
+0 - Fim
+1 - Inclui
+2 - Altera
+3 - Exclui
+4 - Consulta
+
+
+print("Cadastro de Clientes")
+print("0 -Fim")
+print("1 -incluir")
+print("2 -Alterar")
+print("3 -Excluir")
+print("4 -Consultar")
+print(" ")
+
+print("Opção: ")
