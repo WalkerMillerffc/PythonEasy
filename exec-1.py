06e5d8d8f38210b1e772e78ef1c9545b3d313b9b
Walker Miller <walkermillerffc@gmail.com>
2024-09-02 19:31:39 -0300

    exec-1.py

--- exec-1.py
+++ exec-1.py
@@ -1,3 +1,5 @@
-#Exercicio 1  Frase na tela - Implemente um programa que escreve na tela a frase "O primeiro programa a gente nunca esquece!".
+1. Frase na tela - Implemente um programa que imprima na tela a frase "O primeiro programa a gente nunca esquece!".
+
+
 
 print("O Primeiro Programa a Gente Nunc Esquece!")
