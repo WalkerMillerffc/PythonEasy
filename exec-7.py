4330136b9c21ca528d7a1fb225582a9be995a0ad
Walker Miller <walkermillerffc@gmail.com>
2024-09-02 19:59:18 -0300

    exec-7.py

---
+++ exec-7.py
@@ -0,0 +1,30 @@
+7. Pinheiro  - Implemente um programa que desenhe um "pinheiro" na tela, similar ao abaixo.
+Enriqueça o desenho com outros caracteres, simulando enfeites.
+       X
+      XXX
+     XXXXX
+    XXXXXXX
+   XXXXXXXXX
+  XXXXXXXXXXX
+ XXXXXXXXXXXXX
+XXXXXXXXXXXXXXX
+       XX
+       XX
+      XXXX
+
+
+Agora vamos usar outra função chamada input() que libera o teclado para o usuário digitar e, tão logo seja pressionado “ENTER”, armazena o valor digitado na memória.
+
+
+
+print("         x        ")
+print("        x@x       ")
+print("       xxxxx      ")
+print("      xxxxxxx     ")
+print("     xx$xxxx$x    ")
+print("    xxxxxxxxxxx   ")
+print("   xx@xxxxxxx@xx  ")
+print("  x%xxxxxxxxxxx%x ")
+print("         xx       ")
+print("         xx       ")
+print("        xxxx      ")
