8ca4e551f3b2d381cbc6c67d5fc0ba1499da3858
Walker Miller <walkermillerffc@gmail.com>
2024-09-02 19:33:43 -0300

    exec-5.py

--- exec-5.py
+++ exec-5.py
@@ -1,4 +1,12 @@
-#Exercicio 5 Tabela de notas - Você foi contratado por uma escola pra fazer o sistema de boletim dos alunos. Como primeiro passo, escreva um programa que produza a seguinte saída:
+5. Tabela de notas - Você foi contratado por uma escola pra fazer o sistema de boletim dos alunos. Como primeiro passo, escreva um programa que produza a seguinte saída:
+
+ALUNO(A)          NOTA
+=========         =====
+ALINE              9.0  
+MÁRIO              DEZ
+SÉRGIO             4.5    
+SHIRLEY            7.0
+
 
 print("Aluno(A)           Nota")
 print("========           =====")
