c1b99adca645ffe48844c1fdcaa31d5817042d2d
Walker Miller <walkermillerffc@gmail.com>
2024-09-02 19:33:12 -0300

    exec-4.py

--- exec-4.py
+++ exec-4.py
@@ -1,4 +1,11 @@
-#Exercicio 4 - Escrever um programa que imprima a seguinte figura:
+4. Quadrado - Escrever um programa que imprima a seguinte figura:
+
+XXXXX
+X   X
+X   X
+X   X
+XXXXX
+
 
 print("XXXXX")
 print("x   x") 
